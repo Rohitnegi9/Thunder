355f50d6a9282b99d9748947ed2a3bfe3e9c0d75
Rohit ji <negiji@9gmail.com>
2026-07-31 22:59:30 +0530

    hello

--- 03Backend/index.js
+++ 03Backend/index.js
@@ -1,13 +1,29 @@
 
-const github = "Database issue";
 
-// status code: 200
-// more logic 
-if(typeof github == "string"){
+function validator(obj){
+     
+    const {email,age,password,name} = obj;
+    
+    // email: rohit@gmail.com
+    // email: string: @, .com, .in, gmail, yahoo, trim
+
+    // password: "Rohit@345"
+    // Capital, specila,size>=8, number
+
+    // age
+
+    //
 
 }
 
 
-for(let i=0;i<github.length;i++){
-    // ui display karana hai 
-}
+// const signupSchema = z.object({
+//     name: z.string(),
+//     age: z.number(),
+//     email: z.email(),
+//     password: // password
+// })
+
+
+// // data = {name:"Rohit",age: 10, email: 'rohit@gmail.com', password: roifjas}
+// signupSchema.safeParsee(data)
